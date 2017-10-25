7e4a6699c9ff9a743344b021ee5cc18810b9d873
1359704355@qq.com <1359704355@qq.com>
2017-10-25 20:01:33 +0800

    修改部分参数

--- GoMicroDemo/dockerbase/init.sh
+++ GoMicroDemo/dockerbase/init.sh
@@ -4,9 +4,4 @@ mysql -u root -e "CREATE DATABASE gomicro "
 
 R=/data/deploy/gomicro
 cd $R
-for v in `ls vendor`; do
-        if [ "$r" != "vendor.json" ]; then
-            scp -r vendor/$v /data/services/go/src/
-        fi
-done
 bash build_local.sh all
